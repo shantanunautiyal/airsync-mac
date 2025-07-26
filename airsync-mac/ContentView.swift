37482d37bcabfee4bd4d3febf3ee13d74179fb68
sameerasw <sameerasw.com@gmail.com>
2025-07-27 05:26:22 +0530

    Initial Commit

---
+++ airsync-mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  airsync-mac
+//
+//  Created by Sameera Sandakelum on 2025-07-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ airsync-mac/airsync_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  airsync_macApp.swift
+//  airsync-mac
+//
+//  Created by Sameera Sandakelum on 2025-07-27.
+//
+
+import SwiftUI
+
+@main
+struct airsync_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
