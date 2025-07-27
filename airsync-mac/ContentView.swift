7d16a6d6f3545aecfee9cc2a60810481500951d9
sameerasw <sameerasw.com@gmail.com>
2025-07-27 06:25:11 +0530

    Basic connect screen items

--- airsync-mac/ContentView.swift
+++ airsync-mac/ContentView.swift
@@ -9,11 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            VStack{
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.blue)
+                    .frame(width: 200, height: 200)
+
+                Spacer()
+
+                VStack{
+                    ConnectionInfoText()
+                    ConnectionInfoText()
+                    ConnectionInfoText()
+                    ConnectionInfoText()
+                }
+            }
+
+            VStack{
+                VStack{
+                    Label("Device Name", systemImage: "pencil")
+                    TextField("Device Name", text: .constant("Sameera's macBook"))
+                    HStack{
+                        Label("Connect ADB", systemImage: "iphone")
+                        Toggle("", isOn: .constant(true))
+                            .toggleStyle(.switch)
+                    }
+                }
+                .padding()
+                Toggle("Sync devices status", isOn: .constant(true))
+                    .toggleStyle(.switch)
+                    .padding()
+            }
         }
         .padding()
     }
@@ -21,4 +47,15 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+
+struct ConnectionInfoText: View {
+    var body: some View {
+        HStack{
+            Label("IP Address", systemImage: "wifi")
+            Text("192.168.100.1")
+        }
+        .padding(1)
+    }
 }
